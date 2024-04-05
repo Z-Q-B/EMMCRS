3e3fe98db2f750c7c59ae8aa1b9c7ec1a96c95fc
Z-Q-B <109526384+Z-Q-B@users.noreply.github.com>
2024-04-05 15:17:06 +0800

    Update T_ htp.cpp

--- T_ htp.cpp
+++ T_ htp.cpp
@@ -6,6 +6,8 @@
 #define AES_SECURITY 80
 #define MR_PAIRING_SSP
 #include "pairing_1.h"
+#include <random>
+#include <string>
 using namespace std;
 extern "C"
 {
@@ -13,23 +15,46 @@ extern "C"
 #include "mirdef.h"
 }
 #pragma comment(lib,"miracl.lib")
-int main()
-{
-	double sum = 0.0;
-	for (int i = 0; i < 10000; i++) {
-		PFC pfc(AES_SECURITY);
-		Big order = pfc.order();
-		miracl* mip = get_mip();
-		clock_t start, end;
-		double time;
 
-		char text[] = "example text";
-		G1 P;
+std::string generateRandomString(size_t length) {
+	const char charset[] =
+		"0123456789"
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+		"abcdefghijklmnopqrstuvwxyz";
+	const size_t max_index = (sizeof(charset) - 1); 
+	std::random_device rd;
+	std::mt19937 generator(rd());
+	std::uniform_int_distribution<size_t> distribution(0, max_index - 1);
 
-		start = clock();
-		pfc.hash_and_map(P,(char*)text);
-		end = clock();
-		time = double(end - start) / CLOCKS_PER_SEC;
-		sum = sum + time;
+	std::string random_string;
+	for (size_t i = 0; i < length; ++i) {
+		random_string += charset[distribution(generator)];
+	}
+
+	return random_string;
 }
-	printf("%lf", sum/10000 );
+
+int main() {
+    double sum = 0.0;
+    double time=0.0;
+    for (int i = 0; i < 10; i++) {
+        PFC pfc(AES_SECURITY);
+        Big order = pfc.order();
+        miracl* mip = get_mip();
+        clock_t start, end;
+        double time;
+
+        std::string randomText = generateRandomString(12); 
+        G1 P;
+
+        start = clock();
+        pfc.hash_and_map(P, (char*)randomText.c_str());
+        end = clock();
+        time = double(end - start) / CLOCKS_PER_SEC;
+        sum += time;
+    }
+
+    printf("%lf", sum / 10);
+
+    return 0;
+}
